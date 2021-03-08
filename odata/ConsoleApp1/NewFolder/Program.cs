cc8711a6fe1ce1a8a2d96f1bb825361f83cb42d7
mike <jing.cao@gmx.net>
2021-03-08 17:13:19 +0100

    first push

---
+++ odata/ConsoleApp1/NewFolder/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            /* array
+            Console.Write("Geben Sie die gewünschte Anzahl von Werten ein: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            int[] number = new int[n];
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("geben sie die Gewünschte Anzahl von Wert ein:");
+                number[i - 1] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            foreach (var item in number)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            number = null;
+            */
+            // list
+            List<int> number = new List<int>();
+            Console.Write("Geben Sie die gewünschte Anzahl von Werten ein: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("geben sie die Gewünschte Anzahl von Wert ein:");
+                number.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            foreach (var item in number)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
---
+++ odata/liste and arraylist/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace liste_and_arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> number = new List<int>();
+            number.Add(4);
+            number.Add(5);
+            number.AddRange(number);
+            number.Reverse();
+            number.Sort();
+
+            foreach (var item in number)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            ;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
